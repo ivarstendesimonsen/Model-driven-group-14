05968e4b7c6d3d28f07de861643b36e1cc3a6092
olavsro <olavsro@stud.ntnu.no>
2026-09-22 15:47:46 +0200

    Ecore added

--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -3,7 +3,11 @@ open module cardgame {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires org.eclipse.emf.common;
+	requires org.eclipse.emf.ecore;
+	requires org.eclipse.emf.ecore.xmi;
 
 	exports cardgame.core;
+	exports cardgame.rules;
 	exports cardgame.ui;
 }
